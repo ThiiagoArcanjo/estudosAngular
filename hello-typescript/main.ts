ae46cf2fad791e51d70211b1a3aa0345d62b9b05
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2022-03-01 23:18:20 -0300

    Primeiro commit. Da primeira aula até emitindo eventos com output properties

---
+++ hello-typescript/main.ts
@@ -0,0 +1,28 @@
+var minhaVar = "Minha Variavel";
+
+function minhaFunc(x,y){
+    return x + y;
+}
+
+//ES6 ou ES 2015
+let num = 2;
+const Pi= 3.14;
+
+//es6-features.org
+
+var numeros  = [1,2,3];
+numeros.map(function(valor){
+    return valor *2;
+});
+
+numeros.map(valor =>  valor * 2); // ES2015
+
+class Matematica{
+    soma(x,y){
+        return x + y;
+    }
+}
+
+var n1: any = "String"
+n1 = 4;
+
